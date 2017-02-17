fc28bcd7e5d612358dbba008767eb6e42b06b8c7
Milos Lajtman <milos.lajtman@edocu.com>
2017-02-17 10:39:55 +0100

    Hide topic description in add/edit topic form
    Enqueue custom admin styles in admin area.

--- web/app/themes/onlinegeneracia/src/admin.php
+++ web/app/themes/onlinegeneracia/src/admin.php
@@ -22,3 +22,7 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+add_action('admin_enqueue_scripts', function() {
+    wp_enqueue_style('onlinegeneracia/admin-area.css', asset_path('styles/admin-area.css'));
+});
