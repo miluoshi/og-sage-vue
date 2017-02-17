067426f79b840753c8bac90777987b7af2600e5d
Milos Lajtman <milos.lajtman@edocu.com>
2017-02-17 13:39:10 +0100

    Add support for vue single-file components

--- web/app/themes/onlinegeneracia/assets/build/webpack.config.js
+++ web/app/themes/onlinegeneracia/assets/build/webpack.config.js
@@ -37,6 +37,20 @@ let webpackConfig = {
         options: { objectAssign: 'Object.assign' },
       },
       {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+          },
+          // other vue-loader options go here
+        },
+      },
+      {
         test: /\.css$/,
         include: config.paths.assets,
         loader: ExtractTextPlugin.extract({
