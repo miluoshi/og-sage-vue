fa7b188e1282b1e39b7815c38845ee08de6e7715
Milos Lajtman <milos.lajtman@edocu.com>
2017-03-05 17:23:54 +0100

    Set body classes depending on window width

--- web/app/themes/onlinegeneracia/assets/scripts/routes/common.js
+++ web/app/themes/onlinegeneracia/assets/scripts/routes/common.js
@@ -1,6 +1,23 @@
+function setBodyClass($body, className, sizeFrom, sizeTo) {
+  const width = $(window).width()
+
+  if (width >= sizeFrom && width < sizeTo && !$body.hasClass(className)) {
+      $body.removeClass('device-small device-medium device-large')
+      $body.addClass(className)
+  }
+}
+
 export default {
   init() {
     // JavaScript to be fired on all pages
+    var $body = $('body')
+
+    // Switch body classes depending on screen size
+    $(window).on('resize ready', () => {
+      setBodyClass($body, 'device-small', 0, 768)
+      setBodyClass($body, 'device-medium', 768, 1024)
+      setBodyClass($body, 'device-large', 1024, 99999)
+    })
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
