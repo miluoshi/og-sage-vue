139fb4a00e2cbff9aca49d72c95278f51e61c334
Milos Lajtman <milos.lajtman@edocu.com>
2017-03-05 16:20:39 +0100

    Add social icons to footer

--- web/app/themes/onlinegeneracia/templates/partials/footer.blade.php
+++ web/app/themes/onlinegeneracia/templates/partials/footer.blade.php
@@ -7,5 +7,11 @@
         {{ $footer_nenasli_ste_temu['text'] }}
       </a>
     </div>
+
+    <nav class="nav-social-icons">
+      @if (has_nav_menu('social_navigation'))
+          {!! wp_nav_menu(['theme_location' => 'social_navigation', 'menu_class' => 'nav']) !!}
+      @endif
+    </nav>
   </div>
 </footer>
