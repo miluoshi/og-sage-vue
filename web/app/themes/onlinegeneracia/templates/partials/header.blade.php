2b5ed5435e26e93f6827e7707b82fb3088cf21d0
Milos Lajtman <milos.lajtman@edocu.com>
2017-03-05 12:32:23 +0100

    Display defined topics at the end of the navigation

--- web/app/themes/onlinegeneracia/templates/partials/header.blade.php
+++ web/app/themes/onlinegeneracia/templates/partials/header.blade.php
@@ -1,10 +1,25 @@
-<header class="banner">
+<header>
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+
+        <ul class="navigation-topics">
+          @foreach ($navigation_topics as $topic)
+            <li>
+              <a href="{{$topic['url']}}">
+                {{$topic['name']}}
+              </a>
+            </li>
+          @endforeach
+        </ul>
       @endif
     </nav>
+
+    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+  </div>
+
+  <div class="social-icons">
+
   </div>
 </header>
