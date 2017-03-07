29e1047be6628756175178913d745e1e077af2bc
Milos Lajtman <milos.lajtman@edocu.com>
2017-03-07 22:31:56 +0100

    Init Siema carousel

--- web/app/themes/onlinegeneracia/assets/scripts/routes/home.js
+++ web/app/themes/onlinegeneracia/assets/scripts/routes/home.js
@@ -1,6 +1,9 @@
+import initSiema from '../util/initSiema'
+
 export default {
   init() {
     // JavaScript to be fired on the home page
+    initSiema()
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
---
+++ web/app/themes/onlinegeneracia/assets/scripts/util/initSiema.js
@@ -0,0 +1,31 @@
+import $ from 'jquery'
+import Siema from 'siema'
+
+let carousel
+const $body = $('body')
+const carouselSelector = '.topics-index-carousel'
+
+function destroyCarousel(carouselObj) {
+  carouselObj.destroy()
+
+  // Removes 2 levels of parents - the DOM created by Siema plugin
+  $('.topic-card').unwrap()
+  $('.topic-card:first').unwrap()
+}
+
+export default () => {
+  $(window).on('ready resize', () => {
+    if ($body.hasClass('device-small')) {
+      carousel && destroyCarousel(carousel)
+      carousel = undefined
+    } else if (!carousel) {
+      carousel = new Siema({
+        selector: carouselSelector,
+        duration: 350,
+        perPage: {
+            768: 3,
+        },
+      })
+    }
+  })
+}
