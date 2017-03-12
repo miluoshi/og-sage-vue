af20cf4b2d5d15065f9c92bba88a07d5cbed358d
Milos Lajtman <milos.lajtman@edocu.com>
2017-03-12 22:27:25 +0100

    Add data for home page and update template/css accordingly

--- web/app/themes/onlinegeneracia/src/controllers.php
+++ web/app/themes/onlinegeneracia/src/controllers.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
-function default_template_data($data) {
-    $taxonomy_name = 'topic';
+define('App\TAXONOMY_NAME',  'topic');
 
+function default_template_data($data) {
     $data['topics'] = get_terms([
-        'taxonomy' => $taxonomy_name,
+        'taxonomy' => TAXONOMY_NAME,
         'hide_empty' => false,
     ]);
 
@@ -22,16 +22,33 @@ function default_template_data($data) {
     return $data;
 }
 
+function home_page_data($data) {
+    $topics = get_terms([
+        'taxonomy' => TAXONOMY_NAME,
+        'hide_empty' => false,
+    ]);
+
+    $data['topics_cards'] = [];
+    foreach ($topics as $key => $topic) {
+        $term_id = TAXONOMY_NAME . '_' . $topic->term_id;
+        $data['topics_cards'][] = (object)[
+            'index' => $key + 1,
+            'name' => $topic->name,
+            'url' => get_term_link($topic->slug, TAXONOMY_NAME),
+            'cover_photo' => get_field('topic_cover_photo', $term_id),
+            'description' => get_field('topic_description', $term_id),
+        ];
+    }
+
+    return $data;
+}
+
 // Use default_template_data for all page types
-add_filter('sage/template/home/data', 'App\\default_template_data');
 add_filter('sage/template/page/data', 'App\\default_template_data');
 add_filter('sage/template/single/data', 'App\\default_template_data');
 add_filter('sage/template/tax-topic/data', 'App\\default_template_data');
 add_filter('sage/template/error404/data', 'App\\default_template_data');
+add_filter('sage/template/home/data', 'App\\default_template_data');
 
 // HOME page
-add_filter('sage/template/home/data', function($data) {
-
-
-    return $data;
-});
+add_filter('sage/template/home/data', 'App\\home_page_data');
--- web/app/themes/onlinegeneracia/templates/index.blade.php
+++ web/app/themes/onlinegeneracia/templates/index.blade.php
@@ -3,11 +3,17 @@
 @section('content')
   {{-- @include('partials.page-header') --}}
 
-  @if ($topics)
-
+  @if ($topics_cards)
     <div class="topics-index-carousel">
-      @foreach ($topics as $topic)
-          <div class="topic-card">
+      @foreach ($topics_cards as $topic)
+          <div class="topic-card"
+            style="background-image: url('{{ $topic->cover_photo['sizes']['medium_large'] }}');"
+          >
+            <div class="topic-index-wrap">
+              <span class="topics-length">{{ sizeof($topics) }}</span>
+              <span class="topic-index-slash">/</span>
+              <span class="topic-index">{{$topic->index }}</span>
+            </div>
             {{$topic->name}}
           </div>
       @endforeach
