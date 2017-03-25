abd27839acf50d9b64a6efa0b03d47bbe8b7d02a
Milos Lajtman <milos.lajtman@edocu.com>
2017-03-25 12:14:06 +0100

    Set topic cover photo CSS inside <style> tag for small devices

--- web/app/themes/onlinegeneracia/templates/index.blade.php
+++ web/app/themes/onlinegeneracia/templates/index.blade.php
@@ -6,20 +6,32 @@
   @if ($topics_cards)
     <div class="topics-index-carousel">
       @foreach ($topics_cards as $topic)
-          <a class="topic-card"
-            href="{{ $topic->url }}"
-            style="background-image: url('{{ $topic->cover_photo['sizes']['medium_large'] }}');"
-          >
-            <div class="topic-index-wrap">
-              <span class="topics-length">{{ sizeof($topics) }}</span>
-              <span class="topic-index-slash">/</span>
-              <span class="topic-index">@php(printf('%02d', $topic->index))</span>
-            </div>
+        {{-- Load topic cover image for each topic on small device inside CSS styles --}}
+        <style>
+          @media screen and (max-width: 767px) {
+            .topics-index-carousel .topic-{{ $topic->index }} {
+              background-image: url("{{ $topic->cover_photo['sizes']['medium_large'] }}");
+            }
+          }
+        </style>
 
-            <h2 class="topic-name">
-              {{ $topic->name }}
-            </h2>
-          </a>
+        <a class="topic-card topic-{{ $topic->index }}"
+          href="{{ $topic->url }}"
+          data-img-placeholder="{{ $topic->cover_photo['sizes']['placeholder'] }}"
+          data-img-small="{{ $topic->cover_photo['sizes']['medium_large'] }}"
+          data-img-medium="{{ $topic->cover_photo['sizes']['large'] }}"
+          data-img-large="{{ $topic->cover_photo['url'] }}"
+        >
+          <div class="topic-index-wrap">
+            <span class="topics-length">{{ sizeof($topics) }}</span>
+            <span class="topic-index-slash">/</span>
+            <span class="topic-index">@php(printf('%02d', $topic->index))</span>
+          </div>
+
+          <h2 class="topic-name">
+            {{ $topic->name }}
+          </h2>
+        </a>
       @endforeach
     </div>
   @endif
