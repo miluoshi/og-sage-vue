5e7d0ff2299705b995b6f7cfcb7ea068e5d80fc4
Milos Lajtman <milos.lajtman@edocu.com>
2017-04-08 17:27:25 +0200

    Display topic cards on desktop stretched to 100% height and with a border

--- web/app/themes/onlinegeneracia/assets/scripts/util/initTopicCards.js
+++ web/app/themes/onlinegeneracia/assets/scripts/util/initTopicCards.js
@@ -1,19 +1,28 @@
 import {hasClass} from './classNames'
+import throttle from './throttle'
 
 function init() {
+  const headerHeight = document.querySelector('header').offsetHeight
+  const visibleAreaHeight = window.innerHeight - headerHeight
+
   // Init topic cards to 1/3 of container height on mobile view
   if (hasClass(document.body, 'device-small')) {
     let topicCards = document.querySelectorAll('.topic-card')
-    const headerHeight = document.querySelector('header').offsetHeight
-    const visibleAreaHeight = window.innerHeight - headerHeight
 
     topicCards.forEach(card => {
       card.style.height = visibleAreaHeight / 3 + 'px'
     })
   }
+  // or to maximum height on tablet/desktop
+  else {
+    let carousel = document.querySelector('.topics-index-carousel')
+    const footerHeight = document.querySelector('footer').offsetHeight
+
+    carousel.style.height = (visibleAreaHeight - footerHeight) + 'px'
+  }
 }
 
 export default () => {
   window.addEventListener('load', init)
-  window.addEventListener('resize', init)
+  window.addEventListener('resize', throttle(init, 150))
 }
--- web/app/themes/onlinegeneracia/templates/index.blade.php
+++ web/app/themes/onlinegeneracia/templates/index.blade.php
@@ -4,32 +4,34 @@
   {{-- @include('partials.page-header') --}}
 
   @if ($topics_cards)
-    @include('helpers.topic-image-styles')
+    <div class="container">
+      @include('helpers.topic-image-styles')
 
-    <div class="topics-index-carousel">
-      @foreach ($topics_cards as $topic)
+      <div class="topics-index-carousel">
+        @foreach ($topics_cards as $topic)
 
-        <a class="topic-card topic-{{ $topic->index }}"
-          href="{{ $topic->url }}"
-          data-index="{{ $topic->index }}"
-          {{-- data-img-placeholder="{{ $topic->placeholder_uri }}" --}}
-          data-img-small="{{ $topic->cover_photo['sizes']['medium'] }}"
-          data-img-medium="{{ $topic->cover_photo['sizes']['medium_large'] }}"
-          data-img-large="{{ $topic->cover_photo['sizes']['large'] }}"
-          data-img-original="{{ $topic->cover_photo['url'] }}"
-          data-width-original="{{ $topic->cover_photo['width'] }}"
-        >
-          <div class="topic-index-wrap">
-            <span class="topics-length">{{ sizeof($topics) }}</span>
-            <span class="topic-index-slash">/</span>
-            <span class="topic-index">@php(printf('%02d', $topic->index))</span>
-          </div>
+          <a class="topic-card topic-{{ $topic->index }}"
+            href="{{ $topic->url }}"
+            data-index="{{ $topic->index }}"
+            {{-- data-img-placeholder="{{ $topic->placeholder_uri }}" --}}
+            data-img-small="{{ $topic->cover_photo['sizes']['medium'] }}"
+            data-img-medium="{{ $topic->cover_photo['sizes']['medium_large'] }}"
+            data-img-large="{{ $topic->cover_photo['sizes']['large'] }}"
+            data-img-original="{{ $topic->cover_photo['url'] }}"
+            data-width-original="{{ $topic->cover_photo['width'] }}"
+          >
+            <div class="topic-index-wrap">
+              <span class="topics-length">{{ sizeof($topics) }}</span>
+              <span class="topic-index-slash">/</span>
+              <span class="topic-index">@php(printf('%02d', $topic->index))</span>
+            </div>
 
-          <h2 class="topic-name">
-            {{ $topic->name }}
-          </h2>
-        </a>
-      @endforeach
+            <h2 class="topic-name">
+              {{ $topic->name }}
+            </h2>
+          </a>
+        @endforeach
+      </div>
     </div>
   @endif
 
